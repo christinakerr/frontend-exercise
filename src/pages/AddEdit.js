7874ae06fc3ee3d1e2fe6c132a0b1ee37962450e
Christi Kerr <christinakerr394@gmail.com>
2021-03-08 10:06:08 -0600

    Built basic HTML for Add page

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import Button from "../components/Button"
 
 function AddEdit() {
     return (
-        <main>Add/Edit</main>
+        <main>
+            <div>
+                <h2>Add a new job</h2>
+                <h3>Fill out the information for your new job listing.</h3>
+            </div>
+            <div>
+                <form>
+                    <label htmlFor="title">
+                        <h4>Job Title</h4>
+                        <p>What is the name of the role?</p>
+                    </label>
+                    <input type="text" id="title" name="title" placeholder="e.g. Software Engineer"></input>
+                    <label htmlFor="location">
+                        <h4>Location</h4>
+                        <p>Where is this job?</p>
+                    </label>
+                    <input type="text" id="location" name="location" placeholder="e.g. Chicago, IL"></input>
+                    <label htmlFor="sponsorship">
+                        <h4>Sponsorship</h4>
+                        <p>Do you want to promote this job?</p>
+                    </label>
+                    <select id="sponsorship" name="sponsorship">
+                        <option value="free">Free</option>
+                        <option value="sponsored">Sponsored</option>
+                    </select>
+                    <label htmlFor="status">
+                        <h4>Status</h4>
+                        <p>Are you ready to make this job listing public?</p>
+                    </label>
+                    <select id="status" name="status">
+                        <option value="open">Open</option>
+                        <option value="paused">Paused</option>
+                        <option value="closed">Closed</option>
+                    </select>
+                    <br />
+                    <input type="submit" value="Submit"></input>
+                    <Button>Cancel</Button>
+                </form>
+            </div>
+        </main>
     );
 }
 
