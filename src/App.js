fd8bf747ffab2741492e0615f7c8d084271cd8ef
Christi Kerr <christinakerr394@gmail.com>
2021-03-09 11:18:23 -0600

    Added HTML to not found and created route to it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Header from "./components/Header"
 import Container from "./pages/Container";
+import NotFound from "./pages/NotFound"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Container} />
         <Route path="/edit/:id" component={Container} />
         <Route exact path="/add" component={Container} />
+        <Route component ={NotFound} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Button from "../components/Button"
+import { Link } from "react-router-dom"
 
 function NotFound() {
     return (
-        <h1>Not found</h1>
+        <div>
+            <h1>Page Not Found</h1>
+            <Link to="/"><Button>Return to Listings</Button></Link>
+        </div>
     );
 }
 
