1f13768b4c11349d36873edc146312d76fe4c151
Christi Kerr <christinakerr394@gmail.com>
2021-03-09 11:12:19 -0600

    Added basic HTML to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Header() {
     return (
-        <header>Header</header>
+        <header>
+            <img src="https://via.placeholder.com/36" alt="Placeholder avatar"/>
+            <h4>Julie Howard</h4>
+            <h6>Admin</h6>
+        </header>
     );
 }
 
