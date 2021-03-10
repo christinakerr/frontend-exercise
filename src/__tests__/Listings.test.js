075c3388704004cbb3eb1ccfdb6cb01a92d52877
Christi Kerr <christinakerr394@gmail.com>
2021-03-10 09:49:38 -0600

    Moved job list mock to .json file for use in multiple test suites

--- src/__tests__/Listings.test.js
+++ src/__tests__/Listings.test.js
@@ -1,20 +1,10 @@
-import { getByTestId } from '@testing-library/react'
+import jobList from "./jobList.json"
 import Listings from '../pages/Listings';
-import Job from "../components/Job"
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 
-const jobList = [
-    {
-        key: "1",
-        location: "Austin, TX",
-        posted: "10/04/2020",
-        sponsorship: "Free",
-        status: "Open",
-        title: "Product Manager"
-    }
-]
+
 
 test('<Listings /> renders', () => {
     const { getByTestId } = render(<MemoryRouter><Listings /></MemoryRouter>);
