5a1b35633594cf68b2c91e8780da0a50a769dd39
Christi Kerr <christinakerr394@gmail.com>
2021-03-11 15:17:05 -0600

    Styled elements in each job listing

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -7,12 +7,12 @@ function Job(props) {
     return (
         <div data-testid="job" className="job-row job-grid">
             <div className="job-title-col">
-                <div>{props.title}</div>
-                <div>{props.location}</div>
+                <div className="job-item">{props.title}</div>
+                <div className="job-location">{props.location}</div>
             </div>
-            <div className="center">{props.posted}</div>
-            <div className="center">{props.sponsorship}</div>
-            <div className="center">{props.status}</div>
+            <div className="center job-item">{props.posted}</div>
+            <div className="center job-item">{props.sponsorship}</div>
+            <div className="center job-item">{props.status}</div>
             <Link to={"/edit/" + props.id} data-testid="edit-job-btn" className="right"><button className="btn-secondary">Edit</button></Link>
         </div>
     );
