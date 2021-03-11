fdf52385ec21632f9b267138894f56da058f5c59
Christi Kerr <christinakerr394@gmail.com>
2021-03-11 12:01:30 -0600

    Formatted elements in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,11 @@ function Header() {
     return (
         <div className="header-bg">
         <header>
-            <img src="https://via.placeholder.com/36" alt="Placeholder avatar"/>
-            <div className="user">
+                <img src="https://via.placeholder.com/36" alt="Placeholder avatar"/>
+                <div className="user">
                 <h4>Julie Howard</h4>
                 <h6>Admin</h6>
-            </div>
-            
+                </div>
         </header>
         </div>
     );
