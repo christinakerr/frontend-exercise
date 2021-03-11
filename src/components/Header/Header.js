0d4ab0decd0ca8dfd4519b2f16a2e55693459caa
Christi Kerr <christinakerr394@gmail.com>
2021-03-11 11:23:47 -0600

    Styled individual elements in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import "./Header.css"
 
 function Header() {
     return (
+        <div className="header-bg">
         <header>
             <img src="https://via.placeholder.com/36" alt="Placeholder avatar"/>
-            <h4>Julie Howard</h4>
-            <h6>Admin</h6>
+            <div className="user">
+                <h4>Julie Howard</h4>
+                <h6>Admin</h6>
+            </div>
+            
         </header>
+        </div>
     );
 }
 
